fabd891d569d54cb6cb0153839032165df000d39
ax-sh <56112475+ax-sh@users.noreply.github.com>
2026-04-07 01:41:02 +0545

    add icons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ export function add(a: number, b: number): number {
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+// @ts-expect-error not a error
 if (import.meta.main) {
   console.log("Add 2 + 3 =", add(2, 3));
 }
